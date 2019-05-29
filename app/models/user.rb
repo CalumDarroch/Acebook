be0924251b4765cdfc80ca5e7db070939b41b254
mat <matmoon14@gmail.com>
2019-05-29 10:50:53 +0100

    user cant type more than 10 char in the password section

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX }
+            format: { with: URI::MailTo::EMAIL_REGEXP }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 end
